51ff14141d8399bfe2823a93b949b6d29031127b
Kwiatkowska-W <wikikwit2004@gmail.com>
2021-12-08 14:52:05 +0100

    przycisk zmiainy

--- app/src/main/java/xml/example/mojaapka/MainActivity.java
+++ app/src/main/java/xml/example/mojaapka/MainActivity.java
@@ -3,13 +3,21 @@ package xml.example.mojaapka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button).setOnClickListener();
+        btn1=(Button)findViewById(R.id.button);
+
+
+
+        btn1.setOnClickListener(this);
+    }
+    public void onClick(View v) {
     }
 }
